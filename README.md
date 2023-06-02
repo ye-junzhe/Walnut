83caf97eddeaab0124cbdcb12bfa1efc152b216e
Ye Junzhe <junzheyez@gmail.com>
2023-06-02 16:05:18 +0800

    Episode 1

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ![RayTracingExample](https://github.com/ye-junzhe/Images/blob/main/RayTracing/Sphere.png)
 WARNING:\
 The Walnut window management isn't working properly, sometimes the window just disappears and needs to rerun the executable\
-And there's a problem which makes the window too small, but all works OK after stretching
 ```
 sudo sh build.sh # run this if you enter this repo first time
 sh install.sh
--- WalnutApp/src/WalnutApp.cpp
+++ WalnutApp/src/WalnutApp.cpp
@@ -2,52 +2,14 @@
 #include "Walnut/EntryPoint.h"
 
 #include "Walnut/Image.h"
+#include "Walnut/Random.h"
 #include "Walnut/Timer.h"
-
-#include "Renderer.h"
-#include "Camera.h"
-
-#include <glm/gtc/type_ptr.hpp>
 
 using namespace Walnut;
 
 class ExampleLayer : public Walnut::Layer
 {
 public:
-	ExampleLayer()
-		: m_Camera(45.0f, 0.1f, 100.0f)
-	{
-		Material& pinkSphere = m_Scene.Materials.emplace_back();
-		pinkSphere.Albedo = { 1.0f, 0.0f, 1.0f };
-		pinkSphere.Roughness = 0.0f;
-
-		Material& blueSphere = m_Scene.Materials.emplace_back();
-		blueSphere.Albedo = { 0.2f, 0.3f, 1.0f };
-		blueSphere.Roughness = 0.1f;
-
-		{
-			Sphere sphere;
-			sphere.Position = { 0.0f, 0.0f, 0.0f };
-			sphere.Radius = 1.0f;
-			sphere.MaterialIndex = 0;
-			m_Scene.Spheres.push_back(sphere);
-		}
-
-		{
-			Sphere sphere;
-			sphere.Position = { 0.0f, -101.0f, 0.0f };
-			sphere.Radius = 100.0f;
-			sphere.MaterialIndex = 1;
-			m_Scene.Spheres.push_back(sphere);
-		}
-	}
-
-	virtual void OnUpdate(float ts) override
-	{
-		if (m_Camera.OnUpdate(ts))
-			m_Renderer.ResetFrameIndex();
-	}
-
 	virtual void OnUIRender() override
 	{
 		ImGui::Begin("Settings");
@@ -56,43 +18,6 @@ public:
 		{
 			Render();
 		}
-
-		ImGui::Checkbox("Accumulate", &m_Renderer.GetSettings().Accumulate);
-
-		if (ImGui::Button("Reset"))
-			m_Renderer.ResetFrameIndex();
-
-		ImGui::End();
-
-		ImGui::Begin("Scene");
-		for (size_t i = 0; i < m_Scene.Spheres.size(); i++)
-		{
-			ImGui::PushID(i);
-
-			Sphere& sphere = m_Scene.Spheres[i];
-			ImGui::DragFloat3("Position", glm::value_ptr(sphere.Position), 0.1f);
-			ImGui::DragFloat("Radius", &sphere.Radius, 0.1f);
-			ImGui::DragInt("Material", &sphere.MaterialIndex, 1.0f, 0, (int)m_Scene.Materials.size() - 1);
-
-			ImGui::Separator();
-
-			ImGui::PopID();
-		}
-
-		for (size_t i = 0; i < m_Scene.Materials.size(); i++)
-		{
-			ImGui::PushID(i);
-
-			Material& material = m_Scene.Materials[i];
-			ImGui::ColorEdit3("Albedo", glm::value_ptr(material.Albedo));
-			ImGui::DragFloat("Roughness", &material.Roughness, 0.05f, 0.0f, 1.0f);
-			ImGui::DragFloat("Metallic", &material.Metallic, 0.05f, 0.0f, 1.0f);
-
-			ImGui::Separator();
-
-			ImGui::PopID();
-		}
-
 		ImGui::End();
 
 		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
@@ -101,10 +26,8 @@ public:
 		m_ViewportWidth = ImGui::GetContentRegionAvail().x;
 		m_ViewportHeight = ImGui::GetContentRegionAvail().y;
 
-		auto image = m_Renderer.GetFinalImage();
-		if (image)
-			ImGui::Image(image->GetDescriptorSet(), { (float)image->GetWidth(), (float)image->GetHeight() },
-				ImVec2(0, 1), ImVec2(1, 0));
+		if (m_Image)
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
 
 		ImGui::End();
 		ImGui::PopStyleVar();
@@ -116,16 +39,26 @@ public:
 	{
 		Timer timer;
 
-		m_Renderer.OnResize(m_ViewportWidth, m_ViewportHeight);
-		m_Camera.OnResize(m_ViewportWidth, m_ViewportHeight);
-		m_Renderer.Render(m_Scene, m_Camera);
+		if (!m_Image || m_ViewportWidth != m_Image->GetWidth() || m_ViewportHeight != m_Image->GetHeight())
+		{
+			m_Image = std::make_shared<Image>(m_ViewportWidth, m_ViewportHeight, ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewportWidth * m_ViewportHeight];
+		}
+
+		for (uint32_t i = 0; i < m_ViewportWidth * m_ViewportHeight; i++)
+		{
+			m_ImageData[i] = Random::UInt();
+			m_ImageData[i] |= 0xff000000;
+		}
+
+		m_Image->SetData(m_ImageData);
 
 		m_LastRenderTime = timer.ElapsedMillis();
 	}
 private:
-	Renderer m_Renderer;
-	Camera m_Camera;
-	Scene m_Scene;
+	std::shared_ptr<Image> m_Image;
+	uint32_t* m_ImageData = nullptr;
 	uint32_t m_ViewportWidth = 0, m_ViewportHeight = 0;
 
 	float m_LastRenderTime = 0.0f;
